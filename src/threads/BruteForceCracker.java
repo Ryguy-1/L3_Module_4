e0ae4d452a2575cbfadaa942542714f3a5352b58
Ryguy-1 <league@WorthyMini-5.attlocal.net>
2017-12-03 12:57:57 -0800

    brute force cracker need to check it real fast.

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -5,33 +5,63 @@
 // Let's see who can crack the code the fastest!
 
 // Pro-tip: Do not include the creation of the threads as part of the time. 
+package threads;
 
 import java.util.Random;
 
 public class BruteForceCracker {
-	static long code = (long)(new Random().nextDouble() * 1_000_000_000);
-	
+	static long code = (long) (new Random().nextDouble() * 1_000_000_000);
+
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
-	
+
 	public static void main(String[] args) {
 		System.out.println("Starting Brute Force Checker");
+		long threadNumber = 4;
+		long multiple = code/threadNumber;
+		Thread t1 = new Thread(()->{
+			for (int i = 0; 0 < multiple; i++) {
+				while(!checkCode(i));
+			}
+			
+		});
+		Thread t2 = new Thread(()->{
+			for (long i = multiple; i < multiple*2; i++) {
+				while(!checkCode(i));
+			}
+			
+		});
+		Thread t3 = new Thread(()->{
+			for (long i = multiple*2; i < multiple*3; i++) {
+				while(!checkCode(i));
+			
+			}
+		});
+		Thread t4 = new Thread(()->{
+			for (long i = multiple*3; i < multiple*4; i++) {
+				while(!checkCode(i));
+			}
+			
+		});
+		
 		startTime = System.currentTimeMillis();
 		
-		int ctr = 0;
-		while(!checkCode(ctr++));
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 		
 		endTime = System.currentTimeMillis();
 		elapsedTime = (float)(endTime - startTime);
 		elapsedTime /= 1000.f;
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
 	}
-	
-	public static boolean checkCode(long p){
-		if(p == code){
+
+	public static boolean checkCode(long p) {
+		if (p == code) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,22 @@
-
+package threads;
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+
+	public static void main(String[] args) {
+		createThread(50);
+	}
+
+	static void createThread(int x) {
+		if (x > 0) {
+			new Thread(() -> {
+			}).start();
+			createThread(x-1);
+		}
+		System.out.println("Hello from thread " + x);
+	}
 }
